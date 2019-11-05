363887c9a763bf7e4fa9d2360d422993dbc565ad
hsisco <hysensisco@gmail.com>
2019-11-05 13:54:54 -0800

    Add state object to reducer.jsx

--- reducertodo/src/reducers/reducer.jsx
+++ reducertodo/src/reducers/reducer.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const reducer = (state, action) => {
+export const reducer = (state, action) => {
   switch(action.type) {
 
     default:
       return state;
   }
 }
+
+export const stateObj = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  }
+]
+// addItems = hex => {
+//   const newItem = {
+//     hex: hex,
+//     id: Date.now(),
+//     cast: false
+//   };
+//   this.setState({
+//     hexes: [...this.state.hexes, newItem]
+//   });
+// }
