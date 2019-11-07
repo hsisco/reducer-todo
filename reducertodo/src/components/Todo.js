cda6191976c1f037d406ff013f2f130077183310
hsisco <hysensisco@gmail.com>
2019-11-07 15:15:31 -0800

    Update Todo.js

--- reducertodo/src/components/Todo.js
+++ reducertodo/src/components/Todo.js
@@ -1,12 +1,11 @@
 import React from 'react';
 // import './Todo.css'
 
-const Todo = props => {
+const Todo = ({ item, dispatch }) => {
   return (
-    <div
-      className={`hex${props.todo.cast ? ' cast' : ''}`}
-      onClick={() => props.toggleCast(props.todo.id)}>
-      <p>{props.todo.hex}</p>
+    <div className={`item ${item.completed ? 'completed' : ''}`}
+        onClick={() => dispatch({ type: "TOGGLE", payload: item })}>
+      <p>{item.item}</p>
     </div>
   );
 };
