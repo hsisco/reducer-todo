0ec8a481e57211922cc2248ebc920bddd603ed89
hsisco <hysensisco@gmail.com>
2019-11-07 14:57:46 -0800

    Better cases in reducer

--- reducertodo/src/components/TodoForm.js
+++ reducertodo/src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const TodoForm = props => {
-    const [input, setInput] = useState('');
+  const [input, setInput] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
--- reducertodo/src/reducers/reducer.jsx
+++ reducertodo/src/reducers/reducer.jsx
@@ -3,28 +3,43 @@ import React from 'react';
 export const appReducer = (state, action) => {
   switch(action.type) {
     case "ADD":
-      return {...state, item: action.payload};
+      const newItem = {
+        item: action.payload,
+        completed: false,
+        id: Date.now()
+      }
+      return {...state, 
+        item: [...state.item, newItem]};
+      
+    case "TOGGLE":
+        return {
+          ...state,
+          item: state.list.map(item => {
+            if(item.id === action.payload.id) {
+              return {...item, completed: !item.completed}
+            } else {
+              return item;
+            }
+          }
+        )
+      };
+
     case "COMPLETED":
-        return {...state, item: action.payload};  
+      return {
+        ...state,
+        item: state.list.filter(item => {
+          return item.completed === false;
+        })
+      } 
     default:
       return state;
   }
 }
 
-export const initialState = [
+export const initialState = { list: [
   {
     item: 'Learn about reducers',
     completed: false,
     id: Date.now(),
 }
-]
-// addItems = hex => {
-//   const newItem = {
-//     hex: hex,
-//     id: Date.now(),
-//     cast: false
-//   };
-//   this.setState({
-//     hexes: [...this.state.hexes, newItem]
-//   });
-// }
+]}
