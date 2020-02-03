ad148845da9543c867d46e4e80c09344f033e5ce
hsisco <hysensisco@gmail.com>
2020-02-02 19:28:44 -0800

    Add reducer created and imported to TodoForm

--- appity-app/src/components/TodoForm.js
+++ appity-app/src/components/TodoForm.js
@@ -1,10 +1,41 @@
 import React, { useState } from 'react';
 
 const TodoForm = props => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.dispatch({
+      type: "ADD",
+      payload: input
+    })
+    setInput('');
+  }
+
+  const handleChanges = e => {
+    props.dispatch({type: "ADD", payload: e.target.value})
+  };
 
   return(
-
+    <div className="todo-form">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="todo">Add something to your list</label>
+        <input
+          type="text"
+          name="input"
+          value={input}
+          onChange={handleChanges}
+          />
+          <button onClick={()=>{
+            props.dispatch({type: "ADD", payload: input})
+            setInput('')}}>
+            Add</button>
+          <button onClick={()=>props.dispatch({type: "COMPLETED"})}>
+            Clear Completed</button>
+      </form>
+    </div>
   );
 }
+
 
 export default TodoForm;
--- appity-app/src/reducers/Reducer.js
+++ appity-app/src/reducers/Reducer.js
@@ -1,6 +1,13 @@
 export const appReducer = (state, action) => {
   switch(action.type) {
-  
+    case "ADD":
+      const newItem = {
+        item: action.payload,
+        completed: false,
+        id: Date.now()
+      }
+      return {...state, 
+        list: [state.list, newItem]};
   }
 }
 
