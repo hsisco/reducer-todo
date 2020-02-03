c4ce22425420d6a6b3eb637fecbb7d3325fd2c87
hsisco <hysensisco@gmail.com>
2020-02-02 18:50:01 -0800

    Create Reducer.js

---
+++ appity-app/src/reducers/Reducer.js
@@ -0,0 +1,13 @@
+export const appReducer = (state, action) => {
+  switch(action.type) {
+  
+  }
+}
+
+export const initialState = { list: [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: Date.now(),
+  }
+]}
